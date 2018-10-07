9cb6b1fd8fde2d419e9b309ead5a962d3ea7b983
Travis CI User <travis@example.org>
2018-10-07 08:28:34 +0000

    Automatic Travis documentation build

--- master/implementors/failure/trait.Fail.js
+++ master/implementors/failure/trait.Fail.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["failure"] = [];
+implementors["oauth_client"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"oauth_client/struct.HttpStatusError.html\" title=\"struct oauth_client::HttpStatusError\">HttpStatusError</a>",synthetic:false,types:["oauth_client::HttpStatusError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
