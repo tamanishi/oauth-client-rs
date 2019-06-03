37ca7eedd0a00bb21da10f64f9ed2e85c49c4b79
Travis CI User <travis@example.org>
2019-06-03 10:16:06 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -30,7 +30,6 @@ ALIASES["iovec"] = {};
 ALIASES["itoa"] = {};
 ALIASES["language_tags"] = {};
 ALIASES["lazy_static"] = {};
-ALIASES["lazycell"] = {};
 ALIASES["libc"] = {};
 ALIASES["libflate"] = {};
 ALIASES["lock_api"] = {};
